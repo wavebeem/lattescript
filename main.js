4a2061ca5a80a79bf5f1a052cb982b2173af3cd9
Brian Mock <mock.brian@gmail.com>
2012-01-07 21:18:59 -0800

    Actually have some functions now.

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+var _latte = {
+    the_term: document.getElementById("the_term")
+};
+
+var latte = {
+    print: function() {
+        var out = _latte.the_term;
+
+        for (var i = 0; i < arguments.length; i++) {
+            out.innerHTML += String(arguments[i]);
+            out.innerHTML += " ";
+        }
+
+        out.innerHTML += "\n";
+    },
+
+    clearTerminal: function() {
+        var out = _latte.the_term;
+
+        out.innerHTML = "";
+    }
+};
+
+var x = 0;
+
 function on_run_button_clicked(widget) {
-    alert(widget);
+    x += 1;
+    latte.clearTerminal();
+    latte.print(x);
 }
 
 function on_save_button_clicked(widget) {
-    alert(widget);
+    alert("Save!");
 }
