b752ecee576c3f3de6e688cff99f051f846b81d2
Brian Mock <mock.brian@gmail.com>
2012-01-07 22:58:57 -0800

    Some stuff...

--- main.js
+++ main.js
@@ -1,34 +1,63 @@
 var _latte = {
-    the_term: document.getElementById("the_term")
+    the_term: document.getElementById("the_term"),
+    the_code: document.getElementById("the_code"),
+    DEBUG: true,
+    DEBUG_PREFIX: "DEBUG: ",
 };
 
 var latte = {
-    print: function() {
+    write: function() {
         var out = _latte.the_term;
 
-        for (var i = 0; i < arguments.length; i++) {
-            out.innerHTML += String(arguments[i]);
-            out.innerHTML += " ";
-        }
+        var n = arguments.length;
+        var i = 0;
 
-        out.innerHTML += "\n";
+        for (; i < n - 1; i++) {
+            out.value += String(arguments[i]);
+            out.value += " ";
+        }
+        for (; i < n; i++) {
+            out.value += String(arguments[i]);
+        }
     },
 
-    clearTerminal: function() {
+    print: function() {
+        this.write.apply(this, arguments);
+        this.write("\n");
+    },
+
+    debug: function() {
+        if (! _latte.DEBUG)
+            return;
+
+        this.write(_latte.DEBUG_PREFIX);
+        this.print.apply(this, arguments);
+    },
+
+    clear_output: function() {
         var out = _latte.the_term;
 
-        out.innerHTML = "";
+        out.value = "";
     }
 };
 
 var x = 0;
 
-function on_run_button_clicked(widget) {
+function on_test_button_clicked(widget) {
     x += 1;
-    latte.clearTerminal();
-    latte.print(x);
+    latte.clear_output();
+    latte.write(">>> ");
+    latte.print("iteration", "number", x);
+    latte.write("You said \"");
+    latte.write(_latte.the_code.value);
+    latte.print("\"");
 }
 
 function on_save_button_clicked(widget) {
-    alert("Save!");
+    latte.debug("Saved!");
+}
+
+function on_run_button_clicked(widget) {
+    latte.clear_output();
+    eval(_latte.the_code.value);
 }
