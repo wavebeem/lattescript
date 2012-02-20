bdf0b36f1bcc33deebc233d81c23a79daaef29f1
Brian Mock <mock.brian@gmail.com>
2012-02-19 20:07:34 -0800

    Starting to call procedures.

--- test/ls/latte.js
+++ test/ls/latte.js
@@ -3,6 +3,14 @@ var util   = require("util");
 var parser = require("./parser").parser;
 var lexer  = require("./lexer").lexer;
 
+var DEBUG = true;
+
+function debug() {
+    if (DEBUG) {
+        console.log.apply(console, arguments);
+    }
+}
+
 var code = fs.readFileSync("test.latte", "utf8");
 
 parser.yy = {
@@ -11,54 +19,120 @@ parser.yy = {
 
 parser.lexer = lexer;
 parser.yy.parseError = function(err, hash) {
-    console.log("err =", err);
-    console.log("hash =", hash);
+    debug("err =", err);
+    debug("hash =", hash);
 };
 var ast = parser.parse(code);
 
-console.log("[Abstract Syntax Tree]");
-console.log(JSON.stringify(ast, null, 2));
+debug("[Abstract Syntax Tree]");
+debug(JSON.stringify(ast, null, 2));
 
 var funcs = {};
 var procs = {};
 
-var dispatch = {
-    BLOCK: function(node) {
-        for (var n = 0; n < node.length; n++) {
-            run(node[n]);
+procs.print = {
+    type: "PROC",
+    name: {type: "ID", value: "print"},
+    args: [{type: "ID", value: "str"}],
+    vars: [],
+    body: [{
+        type: "JS",
+        js: function() {
+            console.log("Hello, World!");
         }
-    },
+    }]
+};
 
-    WHILE: function(node) {
-        console.log("While loop");
-        for (var n = 0; n < node.statements.length; n++) {
-            run(node.statements[n]);
-        }
-    },
+var dispatch = {};
 
-    PROC_CALL: function(node) {
-        console.log("Calling procedure:", node.name.value);
-        run(procs[node.name.value]);
-    },
-
-    PROC_DEF: function(node) {
-        console.log("Defining procedure:", node.name.value);
-        procs[node.name.value] = node.body;
-    },
-
-    FUNC_DEF: function(node) {
-        console.log("Defining function:", node.name.value);
-        funcs[node.name.value] = node.body;
-    },
-
-    SUB_DEFS: function(node) {
-        for (var n = 0; n < node.sub_defs.length; n++) {
-            run(node.sub_defs[n]);
-        }
-    },
-
-    NOOP: function(node) {
+dispatch.BLOCK = function(node) {
+    for (var n = 0; n < node.length; n++) {
+        run(node[n]);
     }
+};
+
+dispatch.JS = function(node) {
+    node.js();
+}
+
+dispatch.WHILE = function(node) {
+    debug("While loop");
+    for (var n = 0; n < node.statements.length; n++) {
+        run(node.statements[n]);
+    }
+};
+
+dispatch.PROC_CALL = function(node) {
+    debug("Calling procedure:", node.name.value);
+    if (procs[node.name.value]) {
+        call_proc(procs[node.name.value], node.args);
+    }
+    else {
+        debug("Throwing up");
+        throw "up";
+    }
+};
+
+function call_proc(node, args) {
+    var bound_proc = {
+        name: node.name,
+        body: node.body,
+        args: {}
+    };
+
+    for (var i = 0; i < args.length; i++) {
+        var obj = {};
+        obj[node.args[i]] = evaluate(args[i]);
+
+        bound_proc.args[i] = obj;
+    }
+
+    for (var i = 0; i < bound_proc.body.length; i++) {
+        run(bound_proc.body[i]);
+    }
+}
+
+function evaluate(node) {
+    if (node.type === "OP") {
+        // TODO
+        return node
+    }
+
+    return node;
+}
+
+dispatch.PROC_DEF = function(node) {
+    debug("Defining procedure:", node.name.value);
+    procs[node.name.value] = {
+        name: node.name,
+        args: node.args,
+        vars: node.vars,
+        body: node.body
+    };
+};
+
+dispatch.FUNC_DEF = function(node) {
+    debug("Defining function:", node.name.value);
+    var no_return_error = {
+        type: "PROC_CALL",
+        name: {type: "ID", value: "error"},
+        args: ["Reached the end of a function without returning a value."]
+    };
+    procs[node.name.value] = {
+        name: node.name,
+        args: node.args,
+        vars: node.vars,
+        body: node.body.concat(no_return_error)
+    };
+},
+
+dispatch.SUB_DEFS = function(node) {
+    for (var n = 0; n < node.sub_defs.length; n++) {
+        run(node.sub_defs[n]);
+    }
+};
+
+dispatch.NOOP = function(node) {
 };
 
 function run(node) {
@@ -66,22 +140,16 @@ function run(node) {
         dispatch[node.type](node);
     }
     else {
-        console.log("No rule defined to run:", node);
+        debug("No rule defined to run:", node);
     }
-}
+};
 
 run(ast);
 
-//console.log("[Functions]");
-//console.log(funcs);
-
-//console.log("[Procedures]");
-//console.log(procs);
-
-console.log("[Program Execution]");
+debug("[Program Execution]");
 if (procs.main) {
-    run(procs.main);
+    run({type: "PROC_CALL", name: {type: "ID", value: "main"}, args: []});
 }
 else {
-    console.log("Please define a main procedure.");
+    debug("Please define a main procedure.");
 }
