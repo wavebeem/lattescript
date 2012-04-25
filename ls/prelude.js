6e881476893faa28b0c970f225c46f54a7be09a7
Brian Mock <mock.brian@gmail.com>
2012-04-25 12:50:43 -0700

    Added more functions to prelude.

--- ls/prelude.js
+++ ls/prelude.js
@@ -31,17 +31,47 @@ ls.dispatch.define("PROC", "append", ["list", "item"], function (c) {
     }
 });
 
-ls.dispatch.define("FUNC", "not", ["x"], function(c) {
-    var x = get_var("x");
-    if (x.type === "BOOL") {
-        var val = {type: "BOOL", value: !x.value};
-        var ret = {type: "RETURN", value: val}
-        run(ret, c);
-    }
-    else {
-        helpers.error("Can't logically negate a", x.type);
-    }
-});
+var func_maker = function(args) {
+    ls.dispatch.define("FUNC", args.name, ["x"], function(c) {
+        var x = get_var("x");
+        if (x.type === args.in_type) {
+            var val = {type: args.out_type, value: args.func(x.value)};
+            var ret = {type: "RETURN", value: val}
+            run(ret, c);
+        }
+        else {
+            helpers.error("Can't apply", args.name, "to a", x.type);
+        }
+    });
+};
+
+func_maker({name: "not", in_type: "BOOL", out_type: "BOOL", func: function(x) {
+    return !x;
+}});
+
+func_maker({name: "even", in_type: "NUM", out_type: "BOOL", func: function(x) {
+    return x % 2 === 0;
+}});
+
+func_maker({name: "odd", in_type: "NUM", out_type: "BOOL", func: function(x) {
+    return x % 2 === 1;
+}});
+
+func_maker({name: "round", in_type: "NUM", out_type: "NUM", func: function(x) {
+    return Math.round(x);
+}});
+
+func_maker({name: "round", in_type: "NUM", out_type: "NUM", func: function(x) {
+    return Math.round(x);
+}});
+
+func_maker({name: "number", in_type: "TEXT", out_type: "NUM", func: function(x) {
+    return parseFloat(x);
+}});
+
+func_maker({name: "numeric", in_type: "NUM", out_type: "BOOL", func: function(x) {
+    return !isNaN(x);
+}});
 });
 
 ls.prelude();
